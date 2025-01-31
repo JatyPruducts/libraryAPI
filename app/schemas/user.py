b8eb14bffe1a124e78cde7172259a6c033ddf342
Михаил <134938010+JatyPruducts@users.noreply.github.com>
2025-01-31 04:53:30 +0300

    Add Token and UserLogin classes in schemas.user.py.

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -19,7 +19,16 @@ class UserOut(BaseModel):
         orm_mode = True
 
 
+class UserLogin(BaseModel):
+    username: str
+    password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
 # Вызываем update_forward_refs() после всех импортов
 from .lending import LendingOut
-
 UserOut.update_forward_refs()
